9cdcd66ba6a96615ba7f474c1efa837776aa2b42
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-07-15 15:44:49 +0100

    feat: inject dependencies and validate account ID in TicketService
    - Add dependency injection for TicketPaymentService and SeatReservationService.
    - Implement private method to validate account ID using InvalidPurchaseException.
    - Update purchaseTickets to enforce validation.

--- src/pairtest/TicketService.js
+++ src/pairtest/TicketService.js
@@ -1,12 +1,31 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
 
+  #paymentService;
+  #seatService;
+
+  constructor(paymentService = new TicketPaymentService(), seatService = new SeatReservationService()) {
+    this.#paymentService = paymentService;
+    this.#seatService = seatService;
+  }
+
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // throws InvalidPurchaseException
+    this.#validateAccountId(accountId);
+  }
+
+  #validateAccountId(accountId) {
+    if (!Number.isInteger(accountId) || accountId <= 0) {
+      throw new InvalidPurchaseException(
+        "Account ID must be a valid integer greater than zero"
+      );
+    }
   }
 }
