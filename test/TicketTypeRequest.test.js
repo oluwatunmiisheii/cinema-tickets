360a000a66e73f6635df13839b383ffd06106b3f
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-07-15 14:07:46 +0100

    test: test TicketTypeRequest class

---
+++ test/TicketTypeRequest.test.js
@@ -0,0 +1,33 @@
+import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
+
+describe("TicketTypeRequest", () => {
+  test("should throw for invalid ticket type", () => {
+    expect(() => new TicketTypeRequest("SENIOR", 1)).toThrow(
+      "type must be ADULT, CHILD, or INFANT"
+    );
+  });
+
+  test.each([
+    ["string", "2"],
+    ["float", 2.5],
+    ["null", null],
+    ["undefined", undefined],
+  ])("should throw for non-integer ticket count: %s", (_, value) => {
+    expect(() => new TicketTypeRequest("CHILD", value)).toThrow(
+      "noOfTickets must be an integer"
+    );
+  });
+
+  test.each([
+    ["ADULT", 2],
+    ["INFANT", 0],
+    ["CHILD", 5]
+  ])(
+    "should return the correct ticket type and number of tickets for type %s and count %i",
+    (type, count) => {
+      const req = new TicketTypeRequest(type, count);
+      expect(req.getTicketType()).toBe(type);
+      expect(req.getNoOfTickets()).toBe(count);
+    }
+  );
+});
